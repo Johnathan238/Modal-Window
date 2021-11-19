89aed0b782d6e45c0757ffa927cedbd64f67c616
Johnathan238 <jonathanumana86@gmail.com>
2021-11-19 02:45:46 -0500

    Looping over the modal & getting just their text

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsSHowModal = document.querySelectorAll('.show-modal')
+
+for (i = 0; i < btnsSHowModal.length; i++){
+  console.log('====================================');
+  console.log(btnsSHowModal[i].textContent);
+  console.log('====================================');
+}
