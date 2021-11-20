de8b556f34557cef84e4f547b5461cc5e810e641
Johnathan <jonathanumana86@gmail.com>
2021-11-20 02:49:39 -0500

    Looped over the Nodelist to get the textContent from modal

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsSHowModal = document.querySelectorAll('.show-modal')
+
+for(let i =0; i < btnsSHowModal.length; i++){
+    console.log(btnsSHowModal[i].textContent);
+}
