193bf514ecd63edcae97834d2f1591bef5846613
Johnathan238 <jonathanumana86@gmail.com>
2021-12-07 22:03:46 -0500

    not working

--- script.js
+++ script.js
@@ -5,8 +5,32 @@ const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsSHowModal = document.querySelectorAll('.show-modal')
 
-for (i = 0; i < btnsSHowModal.length; i++){
+for (let i = 0; i < btnsSHowModal.length; i++) {
   console.log('====================================');
   console.log(btnsSHowModal[i].textContent);
   console.log('====================================');
 }
+
+
+function spinWords(string) {
+  //TODO Have fun :)
+
+  //From MDN 
+  return string.split("").reverse().join("")
+
+  // My work
+  // for (let i = spinWords().length - 1; i > 0; i -= 1) {
+  //   spinWords().reverse
+  // }
+
+  // let rWords = rWords.reverse()
+
+  // return rWords
+
+
+
+}
+
+console.log('====================================');
+console.log(spinWords("Nani"));
+console.log('====================================');
